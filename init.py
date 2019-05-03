0cdf529d2830d7cb4ab90754fe78d93fba1bebaa
40623224 <40623224@gm.nfu.edu.tw>
2019-05-03 16:33:16 +0800

    Consolidated website & modify site title

--- init.py
+++ init.py
@@ -8,7 +8,7 @@ static_dir = _curdir + "/static"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "CMSimfly"
+    site_title = "2019cdb_t1_g4"
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
